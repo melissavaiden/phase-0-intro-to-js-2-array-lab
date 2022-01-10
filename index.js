f2e3066d8f1ff7f45bbe0daf8701ff09ff2085e3
Melissa Vaiden <melissavaiden@gmail.com>
2022-01-10 13:29:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function  destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const copycat = [...cats, name];
+    return copycat;
+}
+function prependCat(name) {
+    const copycat = [name, ...cats];
+    return copycat;
+}
+function removeLastCat() {
+    const newCats = cats.slice(0, cats.length -1);
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+}
